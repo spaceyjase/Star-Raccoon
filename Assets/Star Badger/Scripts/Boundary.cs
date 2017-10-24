84610702ca73defe4aef52c6d614bfa5d6618fce
Jason Adams <julietmikealpha@gmail.com>
2017-10-24 15:02:03 +0100

    Better movement through rigidbody.

---
+++ Assets/Star Badger/Scripts/Boundary.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class Boundary
+{
+  public float xMin;
+  public float xMax;
+  public float yMin;
+  public float yMax;
+}
--- Assets/Star Badger/Scripts/ShipMovement.cs
+++ Assets/Star Badger/Scripts/ShipMovement.cs
@@ -2,16 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class ShipMovement : MonoBehaviour
 {
   [SerializeField]
   private float movementSpeed = 1.0f;
+  [SerializeField]
+  private float tilt = 1.0f;
+  [SerializeField]
+  private Boundary boundary;
 
-  private void Update()
+  private Rigidbody rb;
+
+  private void Awake()
+  {
+    rb = GetComponent<Rigidbody>();
+  }
+
+  private void FixedUpdate()
   {
     float h = Input.GetAxis("Horizontal");
     float v = Input.GetAxis("Vertical");
 
-    transform.position += new Vector3(h, -v, 0) * movementSpeed * Time.deltaTime;
+    rb.velocity += new Vector3(h, -v, 0) * movementSpeed * Time.deltaTime;
+
+    rb.position = new Vector3(
+      Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+      Mathf.Clamp(rb.position.y, boundary.yMin, boundary.yMax),
+      0.0f
+    );
+
+    rb.rotation = Quaternion.Euler(rb.velocity.y * tilt, 0.0f, rb.velocity.x * tilt);
   }
 }
