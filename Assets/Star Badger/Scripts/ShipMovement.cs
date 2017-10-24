a000c1e85a36e493ba0ec8d473848562df4c361c
Jason Adams <julietmikealpha@gmail.com>
2017-10-24 14:48:50 +0100

    Simple transform movement.

---
+++ Assets/Star Badger/Scripts/ShipMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMovement : MonoBehaviour
+{
+  [SerializeField]
+  private float movementSpeed = 1.0f;
+
+  private void Update()
+  {
+    float h = Input.GetAxis("Horizontal");
+    float v = Input.GetAxis("Vertical");
+
+    transform.position += new Vector3(h, -v, 0) * movementSpeed * Time.deltaTime;
+  }
+}
