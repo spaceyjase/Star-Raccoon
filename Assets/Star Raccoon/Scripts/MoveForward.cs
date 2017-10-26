c7de8ddcf8f2d63c3513560be18299eba1c75780
Jason Adams <julietmikealpha@gmail.com>
2017-10-26 12:10:32 +0100

    Floor.

---
+++ Assets/Star Raccoon/Scripts/MoveForward.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+  [SerializeField]
+  private float speed = 1.0f;
+
+  // Update is called once per frame
+  private void Update()
+  {
+    transform.position -= transform.forward * speed * Time.deltaTime;
+  }
+}
