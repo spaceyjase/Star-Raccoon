f0f679630bf1f8aae2be4df0462f29f8f2a7748d
Jason Adams <julietmikealpha@gmail.com>
2017-10-30 11:52:32 +0000

    Rolling.

---
+++ Assets/Star Raccoon/Scripts/Player/Roll.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roll : MonoBehaviour
+{
+  private void Update()
+  {
+    float h = Input.GetAxis("Roll");
+    var rotation = transform.rotation.eulerAngles;
+    rotation.z = -90 * h;
+    transform.rotation = Quaternion.Euler(rotation);
+  }
+}
