6de4ba8a99ea12731d676d5d2153ace8fd5eeb29
Jason Adams <julietmikealpha@gmail.com>
2017-11-02 16:15:42 +0000

    Initial score implementation.

---
+++ Assets/Star Raccoon/Scripts/AddScoreOnTriggerEnter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddScoreOnTriggerEnter : MonoBehaviour
+{
+  [SerializeField]
+  private int value = 100;
+
+  private void OnTriggerEnter(Collider other)
+  {
+    Debug.Log("Collision with ring center");
+    ScoreManager.Instance.AddScore(value);
+  }
+}
---
+++ Assets/Star Raccoon/Scripts/Managers/ScoreManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+  private int score = 0;
+
+  // Singleton
+  private static ScoreManager instance;
+  public static ScoreManager Instance { get { return instance; } }
+
+  private void Awake()  {    if (instance == null)    {      instance = this;    }    else    {      if (instance != this)      {        Destroy(gameObject);      }    }  }  public void AddScore(int value)
+  {
+    Instance.score += value;
+  }
+  private void OnGUI()
+  {
+    GUILayout.Label("Score: " + ScoreManager.Instance.score.ToString("D6"));
+  }
+}
