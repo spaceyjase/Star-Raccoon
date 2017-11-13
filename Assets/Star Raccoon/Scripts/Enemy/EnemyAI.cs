c70f5c6d8d88728a856e0c939386293fe4c6e49c
Jason Adams <julietmikealpha@gmail.com>
2017-11-13 11:48:57 +0000

    Basic player AI.

---
+++ Assets/Star Raccoon/Scripts/Enemy/EnemyAI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+  [SerializeField]
+  private GameObject player;
+  [SerializeField]
+  private float targetDistance = 10.0f;
+
+  private void Update()
+  {
+    if ((player.transform.position - transform.position).magnitude < targetDistance)
+    {
+      Vector3 newPosition = transform.position;
+      newPosition.z = player.transform.position.z + targetDistance;
+      transform.position = newPosition;
+      var newRotation = transform.rotation;
+      newRotation.y = 0;
+      transform.rotation = newRotation;
+    }
+  }
+}
