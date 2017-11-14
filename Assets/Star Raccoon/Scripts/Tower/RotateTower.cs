53fe379dcfdcef671c70c1279aed1031727a4b1c
Jason Adams <julietmikealpha@gmail.com>
2017-11-14 15:24:56 +0000

    Basic Level 2 stuff.

---
+++ Assets/Star Raccoon/Scripts/Tower/RotateTower.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTower : MonoBehaviour
+{
+  [SerializeField]
+  private float rotationSpeed = 10.0f;
+
+  // Use this for initialization
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    transform.RotateAround(transform.up, -Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime);
+  }
+}
